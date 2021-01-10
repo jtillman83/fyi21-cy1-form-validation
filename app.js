a5f9a8052ae725b20b6d587516761746f528b829
jtillman83 <tillman@gmail.com>
2021-01-10 13:46:45 -0500

    create  err message

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const username = document.getElementById('username')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const password2 = document.getElementById('password2')
-const usernameErr = username.nextElementSibling
 
-// console.log(email)
-// console.log(password)
-// console.log(password2)
+const usernameErr = username.nextElementSibling
+const emailErr = email.nextElementSibling
+const passwordErr = password.nextElementSibling
+const password2Err = password2.nextElementSibling
+
+console.log(emailErr)
+console.log(passwordErr)
+console.log(password2Err)
 
 
 // 2. Add event / modification
@@ -24,6 +28,34 @@ regForm.addEventListener('click', function (e) {
   } else {
     console.log('sending to server')
   }
+
+
+  // 2. Check if the value is empty
+  if (email.value === '') {
+    // 3. If empty, give user some feedback
+    usernameErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
+
+
+  // 2. Check if the value is empty
+  if (password.value === '') {
+    // 3. If empty, give user some feedback
+    usernameErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
+
+
+  // 2. Check if the value is empty
+  if (password2.value === '') {
+    // 3. If empty, give user some feedback
+    usernameErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  }
+
 
   //@TODO: add the validation for email
 
